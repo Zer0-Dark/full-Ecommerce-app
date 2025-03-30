d8e1408ffa745e6e1c58ed4a2cd03dac1b998535
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-30 23:46:04 +0200

    add accounts model

--- product_APIs/users/models.py
+++ product_APIs/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    bio = models.TextField(blank=True)
+    profile_picture =models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
