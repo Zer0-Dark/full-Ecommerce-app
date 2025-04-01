6494a2580d9917719d33710d6c45fd331d0ae7f8
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-04-01 14:00:27 +0200

    add registration api view

--- product_APIs/users/views.py
+++ product_APIs/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import RegistrationSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken 
 
-# Create your views here.
+
+
+class RegistrationAPIView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data= request.data)
+        serializer.is_valid(raise_exception= True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {"refresh":str(token),
+                          "access":str(token.access)}
+        return Response(data= data, status=status.HTTP_201_CREATED )
