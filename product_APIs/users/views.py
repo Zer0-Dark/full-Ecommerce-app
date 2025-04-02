1db4f783e92f5e140af3f41963fbfb9533a1df5e
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-04-02 10:46:10 +0200

    add logout view

--- product_APIs/users/views.py
+++ product_APIs/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status
 from .serializers import RegistrationSerializer, LoginSerializer, CustomUserSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken 
 
@@ -35,3 +35,16 @@ class LoginAPIView(generics.GenericAPIView):
         data["tokens"] = {"refresh":str(token),
                         "access":str(token.access_token)}
         return Response(data= data, status=status.HTTP_200_OK )
+    
+
+class UserLogoutAPIView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
