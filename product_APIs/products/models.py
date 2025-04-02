f53e636e168bf29371b8d731f3e3e5a051b630a9
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-04-02 17:34:24 +0200

    add review model

--- product_APIs/products/models.py
+++ product_APIs/products/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -16,10 +20,12 @@ class SubCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=300)
     description = models.CharField(max_length=500)
-    price = models.DecimalField(decimal_places=2, max_digits=8)
+    price = models.DecimalField(decimal_places=2, max_digits=8, validators=[MinValueValidator(0)])
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name="products")
     subcategory = models.ForeignKey(SubCategory, null=True, on_delete=models.SET_NULL, related_name="products")
-    stock_count = models.IntegerField()
+    stock_count = models.IntegerField(validators=[MinValueValidator(0)])
+    # TODO: make signal to update this on every review relevant to the product 
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1,validators=[MaxValueValidator(5.0), MinValueValidator(0.0)])
     # will not store the images on the database but only the reference because
     # https://www.geeksforgeeks.org/python-uploading-images-in-django/
     image =  models.ImageField(upload_to='images/')
@@ -27,8 +33,13 @@ class Product(models.Model):
     created_at_date = models.DateField(auto_now_add=True)
 
 class Reviews(models.Model):
-    # TODO expand the user model first
-    pass
+    # from zero to five (exact numbers)
+    rating = models.IntegerField( validators=[MaxValueValidator(5), MinValueValidator(0)])
+    review = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    last_update_date = models.DateField(auto_now=True)
+    created_at_date = models.DateField(auto_now_add=True)
 
 class ShoppingCart(models.Model):
     # TODO expand the user model first
